1fa8161271e04400bf10ed9d97099eaa9e292ff7
NidhiDdhande <Nidhi.Dhande@ibm.com>
2026-07-21 08:58:20 +0530

    Add files via upload

---
+++ Rest_Assured/Project/Activity6.java
@@ -0,0 +1,67 @@
+package appium_project;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+import static org.testng.Assert.assertEquals;
+
+public class Activity6 {
+	AndroidDriver driver;
+
+    @BeforeClass
+    public void setup() throws MalformedURLException {
+
+        UiAutomator2Options options = new UiAutomator2Options();
+
+        options.setPlatformName("Android");
+        options.setAutomationName("UiAutomator2");
+        options.setDeviceName("Android Emulator");
+        options.setApp("C:\\Apps\\ToDoList.apk");
+
+        driver = new AndroidDriver(
+                new URL("http://127.0.0.1:4723"), options);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+}
+
+@Test
+public void popupLogin() {
+
+    driver.get("https://training-support.net/webelements");
+
+    driver.findElement(AppiumBy.androidUIAutomator(
+            "new UiScrollable(new UiSelector().scrollable(true))"
+            + ".scrollTextIntoView(\"Popups\")"));
+
+    driver.findElement(AppiumBy.androidUIAutomator(
+            "new UiSelector().text(\"Popups\")")).click();
+
+    driver.findElement(By.id("login-btn")).click();
+
+    driver.findElement(By.id("username"))
+            .sendKeys("admin");
+
+    driver.findElement(By.id("password"))
+            .sendKeys("password");
+
+    driver.findElement(By.id("submit")).click();
+
+    String msg =
+            driver.findElement(By.id("action-confirmation"))
+                    .getText();
+
+    assertEquals(msg,
+            "Welcome Back, admin");
+}
+}
